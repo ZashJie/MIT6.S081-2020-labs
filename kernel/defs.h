980d4c08bae243307597a35409e65165c9f2888e
ZashJie <1030298236@qq.com>
2024-02-22 21:49:33 +0800

    mmap-lab

--- kernel/defs.h
+++ kernel/defs.h
@@ -8,6 +8,7 @@ struct spinlock;
 struct sleeplock;
 struct stat;
 struct superblock;
+struct vma;
 
 // bio.c
 void            binit(void);
@@ -139,6 +140,7 @@ int             argaddr(int, uint64 *);
 int             fetchstr(uint64, char*, int);
 int             fetchaddr(uint64, uint64*);
 void            syscall();
+int             vmatrylazytouch(uint64);
 
 // trap.c
 extern uint     ticks;
@@ -171,6 +173,7 @@ uint64          walkaddr(pagetable_t, uint64);
 int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
+void            vmaunmap(pagetable_t, uint64, uint64, struct vma*);
 
 // plic.c
 void            plicinit(void);
